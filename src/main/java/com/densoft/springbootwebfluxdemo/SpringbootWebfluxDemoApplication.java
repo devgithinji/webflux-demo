c22de68e704f7a754338b5d9dfb5b930fcf529b4
dennis githinji <wakahiad@gmail.com>
2023-03-26 15:18:24 +0300

    api docs

--- src/main/java/com/densoft/springbootwebfluxdemo/SpringbootWebfluxDemoApplication.java
+++ src/main/java/com/densoft/springbootwebfluxdemo/SpringbootWebfluxDemoApplication.java
@@ -1,9 +1,18 @@
 package com.densoft.springbootwebfluxdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring web flux crud example",
+                version = "1.0",
+                description = "sample documentation"
+        )
+)
 public class SpringbootWebfluxDemoApplication {
 
     public static void main(String[] args) {
